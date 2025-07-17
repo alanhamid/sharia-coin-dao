efcd658efb302f8346de31f75e2889b459c74dbf
Alan Hamid <alanhamid13@gmail.com>
2025-07-17 14:43:42 -0400

    Update index.js

--- scr/index.js
+++ scr/index.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import DonationPage from './scgt';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<DonationPage />);
 
